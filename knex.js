336ab791e37eb5e02f532f4a3ed695aca2ea01ad
seeneedeen <seeneedeen.may@dplusonline.com>
2025-01-03 11:23:14 +0700

    [ADD]

--- knex.js
+++ knex.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 const knex = require("knex")({
   client: "mysql2",
   connection: {
     host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "YourStrong@Passw0rd",
-    database: "mydb",
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     charset: "utf8mb4",
   },
   pool: {
