37f5f1ec579695daf74507dd2f0bb322ec85e814
seeneedeen <seeneedeen.may@dplusonline.com>
2025-01-03 11:11:04 +0700

    Revert "[UPDATE]"
    This reverts commit c871c6f9e3d38b6befc8e781d8cfb537abcaa51c.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = 3000;
 const knex = require("./knex");
 
 app.get("/", (req, res) => {
--- knex.js
+++ knex.js
@@ -1,13 +1,11 @@
-require("dotenv").config();
-
 const knex = require("knex")({
   client: "mysql2",
   connection: {
     host: "127.0.0.1",
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    port: 3306,
+    user: "root",
+    password: "YourStrong@Passw0rd",
+    database: "mydb",
     charset: "utf8mb4",
   },
   pool: {
